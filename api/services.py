404824a01088db607df2350c0ae0698cf37e5719
czidisaad <zidisaad.chaima@gmail.com>
2025-12-09 16:05:49 +0100

    fix flake errors

--- api/services.py
+++ api/services.py
@@ -35,7 +35,7 @@ async def make_prediction(
 
     if city_name.lower() not in {"lille", "bordeaux"}:
         raise HTTPException(status_code=400, detail="Ville non prise en charge")
-        
+
     prediction, house_dict = await asyncio.to_thread(_predict, data, request, city_name.lower())
     background_tasks.add_task(log_prediction_for_evidently, house_dict, prediction.prix_m2_estime)
 
@@ -51,7 +51,7 @@ def _predict(house: House,
 
     Args:
         house (House): Les caractéristiques du logement.
-        request (Request): L'objet Request FastAPI, permettant d'accéder aux modèles et scalers chargés dans l'application.
+        request (Request): L'objet Request FastAPI, permettant d'accéder aux modèles et scalers chargés.
         ville (str): Le nom de la ville en minuscules ("lille" ou "bordeaux").
 
     Raises:
@@ -60,18 +60,18 @@ def _predict(house: House,
     Returns:
         Prediction: Le résultat de la prédiction avec le prix estimé, la ville et le nom du modèle utilisé.
     """
-     
+
     house_array = np.array([[house.surface_bati, house.nombre_pieces, house.surface_terrain, house.nombre_lots]])
 
     if house.type_local.lower() == "appartement":
         model = request.app.state.model_a if ville == "lille" else request.app.state.model_a_b
-        scaler_X = request.app.state.scaler_Xa 
+        scaler_X = request.app.state.scaler_Xa
         scaler_y = request.app.state.scaler_ya 
 
     elif house.type_local.lower() == "maison":
         model = request.app.state.model_m if ville == "lille" else request.app.state.model_m_b
-        scaler_X = request.app.state.scaler_Xm 
-        scaler_y = request.app.state.scaler_ym 
+        scaler_X = request.app.state.scaler_Xm
+        scaler_y = request.app.state.scaler_ym
 
     else:
         raise HTTPException(status_code=400, detail="Type de logement non supporté")
@@ -80,14 +80,13 @@ def _predict(house: House,
     output_scaled = model.predict(input_scaled)
     output = scaler_y.inverse_transform(output_scaled.reshape(1, -1))
 
-
     prediction = Prediction(prix_m2_estime=output[0][0], ville_modele=ville.capitalize(), model=type(model).__name__)
 
     # Create dictionary for Evidently
     house_dict = {
-    "Surface reelle bati": house.surface_bati,
-    "Nombre pieces principales": house.nombre_pieces,
-    "Surface terrain": house.surface_terrain,
-    "Nombre de lots": house.nombre_lots,
-    }
+        "Surface reelle bati": house.surface_bati,
+        "Nombre pieces principales": house.nombre_pieces,
+        "Surface terrain": house.surface_terrain,
+        "Nombre de lots": house.nombre_lots,
+        }
     return prediction, house_dict
